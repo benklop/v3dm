f45f2a9d9c6aaf88cdc727baef97882e8697444c
Jonathan Araña Cruz <jonhattan@faita.net>
2013-05-06 11:07:22 +0200

    Settings for JSC3D formatter: height, width, color,...

--- v3dm_jsc3d/v3dm_jsc3d.js
+++ v3dm_jsc3d/v3dm_jsc3d.js
@@ -7,6 +7,12 @@ Drupal.behaviors.JSC3D = {
       var file = settings.v3dm_jsc3d.files[id];
       var viewer = new JSC3D.Viewer(this);
       viewer.setParameter('SceneUrl', file);
+
+      var keys = ['InitRotationX', 'InitRotationY', 'InitRotationZ', 'ModelColor', 'BackgroundColor1', 'BackgroundColor2', 'BackgroundImageUrl', 'RenderMode', 'Definition', 'MipMapping', 'SphereMapUrl'];
+      $.each(keys, function(index, value) {
+        viewer.setParameter(value, settings.v3dm_jsc3d[value]);
+      });
+
       viewer.init();
       viewer.update();
     });
