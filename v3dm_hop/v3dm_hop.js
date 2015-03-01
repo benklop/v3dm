02aeb033feaa6496e811ba2e68d4ac4b12e5ce65
Jonathan Araña Cruz <jonhattan@faita.net>
2015-03-01 10:15:35 +0000

    3DHOP: don't include init.js. It is part of the examples, and uses $ to reference jQuery\!

--- v3dm_hop/v3dm_hop.js
+++ v3dm_hop/v3dm_hop.js
@@ -4,8 +4,6 @@ Drupal.behaviors.hop = {
   attach: function (context, settings) {
     $('.hop').each(function() {
 
-      init3dhop();
-
       var presenter = null;
       var id = $('.hop').attr('id');
       var file = settings.v3dm_hop.files[id];
